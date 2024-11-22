86dbaae6667fe3f095575562cbf849c7c2ebf985
HuilinNi15 <nihuilin15@gmail.com>
2024-11-22 15:39:44 +0100

    delete exercise's statement comment

--- RSA_RW/Code/rsa_rw.py
+++ RSA_RW/Code/rsa_rw.py
@@ -6,28 +6,6 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1
 
-"""
-
-En Atenea encontraréis directorio RSA_RW donde hay una serie de ficheros del tipo:
-    - nombre.apellido_AES.enc que es el resultado de cifrar un fichero determinado con la clave K
-    - nombre.apellido_RSA_RW.enc que es el resultado de cifrar la clave K con la clave pública RSA 
-    que se encuentra en el fichero nombre.apellido_pubkeyRSA_RW.pem.
-
-El fichero cifrado se ha obtenido usando el comando:
-    > openssl enc -e -aes-128-cbc -pbkdf2 -kfile fichero.key -in fichero.txt -out fichero.enc
-El fichero fichero.key que contiene la clave se ha cifrado con el comando:
-    > openssl pkeyutl -encrypt -inkey pubkeyRSA.pem -pubin -in fichero.txt -out fichero.enc
-
-openssl est ́a disponible en https://www.openssl.org. Se instala por defecto en la mayoría de las 
-distribuciones de Linux, por ejemplo en la imagen Linux de la FIB.
-
--- ENUNCIADO --
-Del fichero nombre.apellido_pubkeyRSA_RW.pem hay que extraer la clave pública (openssl puede ayudar),
-factorizar el módulo, calcular la clave privada, escribirla en un fichero en formato PEM (puede ser útil la
-biblioteca Crypto.PublicKey.RSA de python) aunque podéis encontrar otras para cualquier lenguaje de
-vuestra preferencia) y, para acabar, descifrar el fichero usando openssl
-
-"""
 
 modulus_dict = {}
 EXPONENT = 0
@@ -60,10 +38,8 @@ for archivo in os.listdir(carpeta):
                                   {name}, exponent: {exponent} \n another exponent: {EXPONENT}")
 
 
-
 # Selección del usuario
 names = ["huilin.ni", "victor.gesiarz"]  
-
 
 for name in names: 
     my_modulus = modulus_dict[name]
@@ -105,6 +81,7 @@ for name in names:
         pem_file.write(pem)
 
     print("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n")
+
 
 # Comanda openssl para descifrar la clave cifrada del AES:
 # openssl pkeyutl -decrypt -inkey huilin.ni_privatekeyRSA_RW.pem -in huilin.ni_RSA_RW.enc -out huilin.ni_AES_key.txt
