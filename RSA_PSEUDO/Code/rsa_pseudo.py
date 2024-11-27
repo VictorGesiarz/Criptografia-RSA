54fa0e605a0958c1f82984f33bd0d1d47416247c
Victor Gesiarz <victor.gesiarz@estudiantat.upc.edu>
2024-11-27 23:38:12 +0100

    RSA Pseudo almost working

--- RSA_PSEUDO/Code/rsa_pseudo.py
+++ RSA_PSEUDO/Code/rsa_pseudo.py
@@ -5,6 +5,9 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
+from math import sqrt
+from decimal import Decimal, getcontext
+
 """
 En Atenea también encontraréis el directorio RSA pseudo donde hay una serie de ficheros semejantes a los
 anteriores.
@@ -44,24 +47,64 @@ for archivo in os.listdir(carpeta):
                 raise ValueError(f"The exponent of one of the users is different from the rest:\n user: \
                                 {name}, exponent: {exponent} \n another exponent: {EXPONENT}")
 
-name = "huilin.ni"
-n = modulus_dict[name]
-n_bin = bin(n)[2:]
 
-# len(n) = 2048, len(p) y len(q) = 1024
-# len(r) y len(s) = 512 --> 1/4 parte de la long de n
-partition_length = len(n_bin)//4
+names = ['huilin.ni', 'victor.gesiarz']
 
-# Partimos la cadena binaria en 4 partes
-part1 = n_bin[:partition_length] # Primera mitad de r * s
-part2 = n_bin[partition_length:partition_length*2] # Segunda mitad de r * s * (r^2 + s^2)
-part3 = n_bin[partition_length*2:partition_length*3] # Primera mitad de r * s * (r^2 + s^2)
-part4 = n_bin[partition_length*3:] # Segunda mitad de r * s
+getcontext().prec = 1024
+for name in names:
+    n = modulus_dict[name]
+    n_bin = bin(n)[2:]
 
-# Convertimos cada parte a entero
-part1_int = int(part1, 2)
-part2_int = int(part2, 2)
-part3_int = int(part3, 2)
-part4_int = int(part4, 2)
+    if len(n_bin) != 2048: 
+        print("Modulus has a different size than 2048")
+        padding_length = 2048 - len(n_bin)
+        if padding_length < 0: 
+            raise ValueError("Modulus has a size bigger than 2048")
+        padding = "0" * padding_length
+        n_bin = padding + n_bin
 
-print(part1_int)
+    # len(n) = 2048, len(p) y len(q) = 1024
+    # len(r) y len(s) = 512 --> 1/4 parte de la long de n
+    partition_length = len(n_bin) // 4
+
+    # Partimos la cadena binaria en 4 partes
+    part1 = n_bin[:partition_length] # Primera mitad de r * s
+    part2 = n_bin[partition_length:partition_length*2] # Segunda mitad de r * s * (r^2 + s^2)
+    part3 = n_bin[partition_length*2:partition_length*3] # Primera mitad de r * s * (r^2 + s^2)
+    part4 = n_bin[partition_length*3:] # Segunda mitad de r * s
+
+    combinations = ['00', '01', '10', '11']
+    r_found = False
+    for combination in combinations: 
+
+        print(f"Part1: {part1}\n")
+
+        part1 = part1[:-2] + combination
+        r_s = part1 + part4
+        s_r = part4 + part1
+        middle = "1" + part2 + part3
+        r2_s2 = bin(int(middle, 2) - int(s_r, 2))
+
+        x = Decimal(int(r_s, 2))
+        y = Decimal(int(r2_s2, 2))
+
+        discriminant = y**2 - 4*x**2
+        if discriminant < 0:
+            print("Continue")
+            continue
+        t = (y + discriminant.sqrt()) / 2
+        r = t.sqrt()
+        
+        s = x / r
+        r_bin = bin(int(r))[2:]
+        s_bin = bin(int(s))[2:]
+        p_bin = r_bin + s_bin
+        q_bin = s_bin + r_bin
+        p = int(p_bin, 2)
+        q = int(q_bin, 2)
+        n = p * q 
+
+        if n == modulus: 
+            print("   P and Q FOUND! p * q = modulus correctly calculated\n")
+
+    print(f'For user {name}:\n    - P: {p}\n    - Q: {q}\n')
